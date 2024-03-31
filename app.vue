070157e0309619702ee737a2a3b873373e526092
KalpasWang <wf6212@gmail.com>
2024-03-31 22:59:43 +0800

    initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <ElButton @click="ElMessage('Hello')">button</ElButton>
+    <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+    <LazyElButton type="warning">lazy button</LazyElButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ElMessage } from "element-plus";
+</script>
