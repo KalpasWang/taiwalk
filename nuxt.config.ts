8a6a4e3f7a1c2b8394e2fa515fed51ce9a29f89d
KalpasWang <wf6212@gmail.com>
2024-04-15 23:07:47 +0800

    refactor to use sass to customize theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@element-plus/nuxt"]
-})
+  modules: ["@element-plus/nuxt", "@nuxtjs/google-fonts"],
+  css: ["~/assets/index.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/element/index.scss" as *;',
+        },
+      },
+    },
+  },
+  elementPlus: {
+    icon: "ElIcon",
+    importStyle: "scss",
+  },
+  googleFonts: {
+    families: {
+      "Noto Sans TC": "200..700",
+      "Noto Sans": "200..700",
+    },
+  },
+});
